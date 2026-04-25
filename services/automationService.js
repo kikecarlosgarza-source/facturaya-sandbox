73b1409840652fba6688549b73d6b95893268174
FIVAL020 <fival020@FIVAL020s-iMac.local>
2026-04-25 01:08:11 -0600

    fix DNS en Playwright para Render

--- services/automationService.js
+++ services/automationService.js
@@ -65,7 +65,7 @@ class PortalAutomationService {
 
     let browser;
     try {
-      browser = await chromium.launch({ headless: true });
+      browser = await chromium.launch({ headless: true, args: ['--dns-prefetch-disable', '--no-sandbox', '--disable-setuid-sandbox'] });
       const context = await browser.newContext({
         userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
         viewport: { width: 390, height: 844 }
@@ -172,7 +172,7 @@ class PortalAutomationService {
   async solicitarFacturaURLDirecta(url, ticketData, perfil) {
     let browser;
     try {
-      browser = await chromium.launch({ headless: true });
+      browser = await chromium.launch({ headless: true, args: ['--dns-prefetch-disable', '--no-sandbox', '--disable-setuid-sandbox'] });
       const page = await (await browser.newContext()).newPage();
       page.setDefaultTimeout(60000);
 
