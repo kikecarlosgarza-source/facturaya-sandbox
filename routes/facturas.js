7eab8426f86aec4dcc074c82eab3beb5c0bbb34a
FIVAL020 <fival020@FIVAL020s-iMac.local>
2026-05-04 00:00:26 -0600

    Bandeja y OXXO Gas al agente visual - no hardcode

--- routes/facturas.js
+++ routes/facturas.js
@@ -7,7 +7,7 @@ const { procesarConAgente } = require('../services/agentService');
 const db = require('../db/database');
 
 // Portales que tienen automation hardcodeada
-const PORTALES_HARDCODED = ['home depot', 'petro', 'bandeja', 'oxxo gas'];
+const PORTALES_HARDCODED = ['home depot', 'petro'];
 
 function usarAgente(establecimiento) {
   const n = (establecimiento || '').toLowerCase();
